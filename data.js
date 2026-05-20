5545a9131a1f68e007505ebcc857a7f88d1a127e
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-20 22:29:47 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,43 @@
 // data.js
-export const alphas = {
-    // Fleischfresser
-    "Hyaenodon": { type: "Fleischfresser", skillRate: 0.5, skillVal: 0.1, skillType: "heal" },
-    "Jaguarundi": { type: "Fleischfresser", skillRate: 0.6, skillVal: 0.3, skillType: "damage" },
-    "Smilodon": { type: "Fleischfresser", skillRate: 0.7, skillVal: 0.2, skillType: "damage" },
-    
-    // Allesfresser
-    "Geierschildkröte": { type: "Allesfresser", skillRate: 0.5, skillVal: 0.4, skillType: "damage" },
-    "Klapperschlange": { type: "Allesfresser", skillRate: 0.6, skillVal: 0.4, skillType: "damage" },
-    "Kodiakbär": { type: "Allesfresser", skillRate: 0.3, skillVal: 3.98, skillType: "damage" },
 
-    // Pflanzenfresser
-    // Hier kannst du künftig deine Pflanzenfresser-Alphas eintragen
+export const alphas = {
+    "Fleischfresser": {
+        "S1": {
+            "Tibetdogge": {
+                skill1: { type: "aura", effect: "reduce_enemy_atk", value: 0.25, reduce_enemy_alpha_dmg: 0.25, target_bonus: "third_row", bonus_value: 1.0 },
+                skill2: { type: "passive", effect: "increase_def", value: 0.38 },
+                skill3: { type: "passive", effect: "increase_atk", value: 0.38 },
+                skill4: { type: "active", chance: 0.7, dmg: 200, reduce_enemy_alpha_dmg: 0.3 },
+                skill5: { type: "passive", effect: "increase_hp", value: 0.15 },
+                skill6: { type: "special", rounds: 3, reduce_enemy_alpha_dmg: 0.3, heal: 4.6 }
+            }
+        },
+        "S2": {},
+        "S3": {},
+        "S4": {}
+    },
+    "Allesfresser": {
+        "S1": {},
+        "S2": {},
+        "S3": {},
+        "S4": {}
+    },
+    "Pflanzenfresser": {
+        "S1": {},
+        "S2": {},
+        "S3": {},
+        "S4": {}
+    }
 };
 
 export const beasts = {
-    // Fleischfresser
-    "Nordwestlicher Wolf": { type: "Fleischfresser", atk: 120, def: 30, hp: 1000 },
-    
-    // Allesfresser
-    "Sumatra-Orang-Utan": { type: "Allesfresser", atk: 90, def: 60, hp: 1500 },
-    
-    // Pflanzenfresser
-    "Takin": { type: "Pflanzenfresser", atk: 60, def: 120, hp: 2500 }
+    "Fleischfresser": {
+        "Nordwestlicher Wolf": { atk: 120, def: 30, hp: 1000 }
+    },
+    "Allesfresser": {
+        "Sumatra-Orang-Utan": { atk: 90, def: 60, hp: 1500 }
+    },
+    "Pflanzenfresser": {
+        "Takin": { atk: 60, def: 120, hp: 2500 }
+    }
 };
