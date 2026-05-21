12fc8d676288f50c4acfdc050d8216eee16b093e
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-21 23:47:38 +0200

    Update omnivores.js

--- data/alphas/omnivores.js
+++ data/alphas/omnivores.js
@@ -3,6 +3,7 @@
  */
 
 const Omnivores = {
+  // --- S1 Allesfresser ---
   "Geierschildkroete": {
     "id": "geierschildkroete_01",
     "class": "Allesfresser",
@@ -26,6 +27,37 @@ const Omnivores = {
         "type": "active_combat_per_round", 
         "range": 2,
         "effects": [{ "target": "enemy_front_row", "type": "alpha_damage", "multiplier": 2.30, "scaling": { "type": "per_alpha_level", "value": 0.01 } }] 
+      }
+    }
+  },
+  "Klapperschlange": {
+    "id": "klapperschlange_01",
+    "class": "Allesfresser",
+    "skills": {
+      "skill1": { 
+        "name": "Vergiftungsinfusion", 
+        "type": "active_combat", 
+        "range": 4, 
+        "effects": [{ "target": "one_random_enemy_row", "type": "apply_poison", "chance": 0.60, "duration": 2, "damage_total": 1.50 }] 
+      },
+      "skill2": { "name": "Bestien-Verteidigung", "type": "passive_stat", "effects": [{ "type": "def_bonus", "value": 0.38 }] },
+      "skill3": { "name": "Bestien-Angriff", "type": "passive_stat", "effects": [{ "type": "atk_bonus", "value": 0.38 }] },
+      "skill4": { 
+        "name": "Neurotoxin", 
+        "type": "active_combat", 
+        "range": 5, 
+        "preparation_rounds": 1, 
+        "effects": [
+          { "target": "two_random_enemy_units", "type": "alpha_damage", "chance": 0.80, "multiplier": 3.50, "scaling": { "type": "per_alpha_level", "value": 0.01 } },
+          { "target": "two_random_enemy_units", "type": "apply_silence_if_poisoned", "duration": 1 }
+        ] 
+      },
+      "skill5": { "name": "Basis-LP", "type": "passive_stat", "effects": [{ "type": "hp_bonus", "value": 0.15 }] },
+      "skill6": { 
+        "name": "Gruseliger Killer", 
+        "type": "combat_buff", 
+        "duration": 4, 
+        "effects": [{ "target": "all_friendly_omnivore_units", "type": "skill_dmg_buff", "value": 0.35, "scaling": { "type": "per_alpha_level", "value": 0.01 } }] 
       }
     }
   }
