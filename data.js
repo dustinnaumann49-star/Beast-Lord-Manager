7cfe7f0cd8cd5e95ed6fcb0029a3f1442cfc3acb
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-21 19:46:53 +0200

    Update data.js

--- data.js
+++ data.js
@@ -11,9 +11,7 @@ const Alphas = {
       "skill1": {
         "name": "Hartnäckiger Kampf",
         "type": "passive",
-        "condition": { 
-          "position": "front_row" 
-        },
+        "condition": { "position": "front_row" },
         "effects": [
           { "target": "self_row", "type": "dmg_reduction", "value": 0.30 },
           { "target": "back_row", "type": "atk_buff_base", "value": 1.00 }
@@ -41,8 +39,42 @@ const Alphas = {
         ]
       }
     }
+  },
+  "Jaguarundi": {
+    "id": "jaguarundi_01",
+    "class": "Fleischfresser",
+    "skills": {
+      "skill1": {
+        "name": "Geschicklichkeit",
+        "type": "passive_start_of_battle",
+        "effects": [
+          { "target": "self_row", "type": "speed_buff", "value": 100 },
+          { "target": "self_row", "type": "normal_atk_dmg_buff_base", "value": 0.80 }
+        ]
+      },
+      "skill4": {
+        "name": "Schwachstellenfang",
+        "type": "trigger",
+        "trigger": "after_normal_attack",
+        "chance": 0.70,
+        "range": 4,
+        "effects": [
+          { "target": "enemy_front_row", "type": "alpha_damage", "multiplier": 2.50, "scaling": { "type": "per_alpha_level", "value": 0.01 } },
+          { "target": "enemy_front_row", "type": "debuff_increase_taken_skill_dmg", "value": 0.30, "scaling": { "type": "per_alpha_level", "value": 0.01 } }
+        ]
+      },
+      "skill6": {
+        "name": "Schneller Stoß",
+        "type": "active_round_start",
+        "chance": 0.60,
+        "duration": 1,
+        "range": 2,
+        "effects": [
+          { "target": "two_random_friendly_rows", "type": "add_extra_normal_attack", "value": 1 }
+        ]
+      }
+    }
   }
-  // Hier werden weitere Alphas nach dem gleichen Schema ergänzt
 };
 
 // Export für die Engine-Nutzung
