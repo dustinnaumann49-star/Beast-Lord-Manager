bf8d737d36fe0aed46c54129ffe42d4eedc4f702
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-22 15:38:47 +0200

    Update carnivore_troops.js

--- data/troops/carnivore_troops.js
+++ data/troops/carnivore_troops.js
@@ -1,1 +1,98 @@
+/**
+ * Fleischfresser-Truppen (T1 - T10)
+ */
 
+const CarnivoreTroops = {
+  "T1": {
+    "Rotfuchs": {
+      "stats": {
+        "atk": 15,
+        "def": 6,
+        "hp": 3
+      }
+    }
+  },
+  "T2": {
+    "Schabrackenschakal": {
+      "stats": {
+        "atk": 21,
+        "def": 9,
+        "hp": 3
+      }
+    }
+  },
+  "T3": {
+    "AfrikanischerWildhund": {
+      "stats": {
+        "atk": 28,
+        "def": 13,
+        "hp": 4
+      }
+    }
+  },
+  "T4": {
+    "Schakal": {
+      "stats": {
+        "atk": 37,
+        "def": 17,
+        "hp": 5
+      }
+    }
+  },
+  "T5": {
+    "Mähnenwolf": {
+      "stats": {
+        "atk": 48,
+        "def": 20,
+        "hp": 7
+      }
+    }
+  },
+  "T6": {
+    "Kojote": {
+      "stats": {
+        "atk": 60,
+        "def": 26,
+        "hp": 8
+      }
+    }
+  },
+  "T7": {
+    "Hyäne": {
+      "stats": {
+        "atk": 73,
+        "def": 34,
+        "hp": 10
+      }
+    }
+  },
+  "T8": {
+    "Polarwolf": {
+      "stats": {
+        "atk": 89,
+        "def": 38,
+        "hp": 12
+      }
+    }
+  },
+  "T9": {
+    "NordwestlicherWolf": {
+      "stats": {
+        "atk": 105,
+        "def": 49,
+        "hp": 13
+      }
+    }
+  },
+  "T10": {
+    "BritishColumbiaWolf": {
+      "stats": {
+        "atk": 124,
+        "def": 53,
+        "hp": 17
+      }
+    }
+  }
+};
+
+export default CarnivoreTroops;
