ea20fe14520953a2f8c20eea98c9f9ee2419a01b
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-22 16:09:13 +0200

    Update omnivore_troops.js

--- data/troops/omnivore_troops.js
+++ data/troops/omnivore_troops.js
@@ -1,1 +1,58 @@
+/**
+ * Allesfresser-Truppen (T1 - T10)
+ */
 
+const OmnivoreTroops = {
+  "T1": {
+    "Makake": {
+      "stats": { "atk": 11, "def": 8, "hp": 4, "speed": 2.0 }
+    }
+  },
+  "T2": {
+    "Stumpfnasennaffe": {
+      "stats": { "atk": 16, "def": 11, "hp": 4, "speed": 1.9 }
+    }
+  },
+  "T3": {
+    "Marmoset": {
+      "stats": { "atk": 21, "def": 15, "hp": 6, "speed": 1.8 }
+    }
+  },
+  "T4": {
+    "RoterBrüllaffe": {
+      "stats": { "atk": 31, "def": 17, "hp": 7, "speed": 1.7 }
+    }
+  },
+  "T5": {
+    "Siamang": {
+      "stats": { "atk": 40, "def": 22, "hp": 9, "speed": 1.6 }
+    }
+  },
+  "T6": {
+    "Guinea-Pavian": {
+      "stats": { "atk": 46, "def": 32, "hp": 11, "speed": 1.5 }
+    }
+  },
+  "T7": {
+    "Mandrill": {
+      "stats": { "atk": 61, "def": 34, "hp": 13, "speed": 1.4 }
+    }
+  },
+  "T8": {
+    "Schimpanse": {
+      "stats": { "atk": 67, "def": 47, "hp": 15, "speed": 1.3 }
+    }
+  },
+  "T9": {
+    "Sumatra-Orang-Utan": {
+      "stats": { "atk": 87, "def": 49, "hp": 18, "speed": 1.2 }
+    }
+  },
+  "T10": {
+    "WestlicherFlachlandgorilla": {
+      "stats": { "atk": 94, "def": 65, "hp": 21, "speed": 1.1 }
+    }
+  }
+};
+
+export default OmnivoreTroops;
