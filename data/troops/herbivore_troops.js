943b8026fd1b91b8ad3eb817c2fc92f714c82e99
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-22 15:50:46 +0200

    Update herbivore_troops.js

--- data/troops/herbivore_troops.js
+++ data/troops/herbivore_troops.js
@@ -1,1 +1,58 @@
+/**
+ * Pflanzenfresser-Truppen (T1 - T10)
+ */
 
+const HerbivoreTroops = {
+  "T1": {
+    "Impala": {
+      "stats": { "atk": 9, "def": 11, "hp": 4, "speed": 2.0 }
+    }
+  },
+  "T2": {
+    "Argali": {
+      "stats": { "atk": 13, "def": 15, "hp": 5, "speed": 1.9 }
+    }
+  },
+  "T3": {
+    "Alpensteinbock": {
+      "stats": { "atk": 18, "def": 20, "hp": 7, "speed": 1.8 }
+    }
+  },
+  "T4": {
+    "Markhor": {
+      "stats": { "atk": 23, "def": 27, "hp": 8, "speed": 1.7 }
+    }
+  },
+  "T5": {
+    "Oryxantilope": {
+      "stats": { "atk": 32, "def": 33, "hp": 10, "speed": 1.6 }
+    }
+  },
+  "T6": {
+    "Elenantilope": {
+      "stats": { "atk": 38, "def": 44, "hp": 13, "speed": 1.5 }
+    }
+  },
+  "T7": {
+    "Gnu": {
+      "stats": { "atk": 46, "def": 53, "hp": 15, "speed": 1.4 }
+    }
+  },
+  "T8": {
+    "AfrikanischerBüffel": {
+      "stats": { "atk": 59, "def": 61, "hp": 17, "speed": 1.3 }
+    }
+  },
+  "T9": {
+    "Takin": {
+      "stats": { "atk": 70, "def": 73, "hp": 20, "speed": 1.2 }
+    }
+  },
+  "T10": {
+    "AmerikanischerBison": {
+      "stats": { "atk": 77, "def": 90, "hp": 25, "speed": 1.1 }
+    }
+  }
+};
+
+export default HerbivoreTroops;
