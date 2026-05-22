2ffa30353d32c8f5c634d2abe7429ada4a002990
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-22 16:23:50 +0200

    Update gameEngine.js

--- engine/gameEngine.js
+++ engine/gameEngine.js
@@ -1,1 +1,15 @@
+// Importe der Truppendaten
+import CarnivoreTroops from '../data/troops/carnivore_troops.js';
+import HerbivoreTroops from '../data/troops/herbivore_troops.js';
+import OmnivoreTroops from '../data/troops/omnivore_troops.js';
 
+// Importe der Alpha-Daten
+import CarnivoreAlphas from '../data/alphas/carnivores.js';
+import HerbivoreAlphas from '../data/alphas/herbivores.js';
+import OmnivoreAlphas from '../data/alphas/omnivores.js';
+
+const GameEngine = {
+    // Hier folgt die Logik, die auf diese 6 Datenquellen zugreift
+};
+
+export default GameEngine;
