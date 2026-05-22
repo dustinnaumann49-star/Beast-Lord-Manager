815f6147499628cab2e1f05f4fd26d4668668ef3
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-05-22 14:36:23 +0200

    Update herbivores.js

--- data/alphas/herbivores.js
+++ data/alphas/herbivores.js
@@ -34,5 +34,29 @@ const herbivores = {
       "skill5": { "name": "Bestien-Angriff", "type": "passive_attribute_buff", "effects": [{ "target": "beast_units", "type": "atk_buff", "value": 0.38 }] },
       "skill6": { "name": "Vorteilhafte Verfolgung", "type": "active_combat_buff_and_dot", "range": 5, "effects": [{ "target": "self_unit", "type": "herbivore_atk_buff", "value": 0.10 }, { "target": "two_random_enemy_units", "type": "dot_damage", "multiplier": 1.00, "scaling": { "type": "per_alpha_level", "value": 0.02 }, "frequency": "each_round" }] }
     }
+  },
+  "Doedicurus": {
+    "id": "doedicurus_01",
+    "class": "Pflanzenfresser",
+    "skills": {
+      "skill1": { "name": "Vollständige Verteidigung", "type": "pre_war_defensive", "range": 1, "effects": [{ "target": "self_unit", "type": "defense_stance", "duration": 3, "restriction": "cannot_normal_attack", "dmg_reduction": 0.50 }] },
+      "skill2": { "name": "Bestien-Verteidigung", "type": "passive_attribute_buff", "effects": [{ "target": "beast_units", "type": "def_buff", "value": 0.48 }] },
+      "skill3": { "name": "Bestien-Angriff", "type": "passive_attribute_buff", "effects": [{ "target": "beast_units", "type": "atk_buff", "value": 0.48 }] },
+      "skill4": { "name": "Lautloser Schlag", "type": "active_combat", "range": 3, "effects": [{ "target": "one_random_enemy_row", "type": "alpha_damage", "chance": 0.50, "multiplier": 2.20, "scaling": { "type": "per_alpha_level", "value": 0.01 } }, { "target": "one_random_enemy_row", "type": "silence", "chance": 0.50, "duration": 1 }] },
+      "skill5": { "name": "Lebenspunkte-Bonus", "type": "passive_attribute_buff", "effects": [{ "target": "self_unit", "type": "hp_buff", "value": 0.20 }] },
+      "skill6": { "name": "Gewaltiger Schwanz", "type": "active_combat_phase", "range": 3, "effects": [{ "target": "two_random_enemy_rows", "type": "phased_damage", "rounds": [2, 4, 6], "multiplier": 3.00, "scaling": { "type": "per_alpha_level", "value": 0.01 } }, { "target": "two_random_enemy_rows", "type": "dmg_taken_debuff_if_cc", "value": 0.30, "duration": 2 }] }
+    }
+  },
+  "Wollnashorn": {
+    "id": "wollnashorn_01",
+    "class": "Pflanzenfresser",
+    "skills": {
+      "skill1": { "name": "Brutale Zerstörung", "type": "active_combat", "range": 5, "effects": [{ "target": "highest_strength_enemy_unit", "type": "alpha_damage", "chance": 0.50, "multiplier": 3.50, "scaling": { "type": "per_alpha_level", "value": 0.01 } }, { "target": "highest_strength_enemy_unit", "type": "suppress", "chance": 0.50, "duration": 1 }] },
+      "skill2": { "name": "Bestien-Verteidigung", "type": "passive_attribute_buff", "effects": [{ "target": "beast_units", "type": "def_buff", "value": 0.48 }] },
+      "skill3": { "name": "Bestien-Angriff", "type": "passive_attribute_buff", "effects": [{ "target": "beast_units", "type": "atk_buff", "value": 0.48 }] },
+      "skill4": { "name": "Rasendes Fegen", "type": "active_combat_phase", "range": 5, "effects": [{ "target": "all_enemy_units", "type": "phased_damage", "rounds": [1, 3, 5, 7], "chance": 0.60, "multiplier": 2.50, "scaling": { "type": "per_alpha_level", "value": 0.01 } }] },
+      "skill5": { "name": "Bestien-Angriff", "type": "passive_attribute_buff", "effects": [{ "target": "beast_units", "type": "atk_buff", "value": 0.48 }] },
+      "skill6": { "name": "Impulsschlag", "type": "active_combat", "range": 5, "effects": [{ "target": "one_random_enemy_row", "type": "dot_damage", "multiplier": 2.50, "scaling": { "type": "per_alpha_level", "value": 0.01 } }, { "target": "one_random_enemy_row", "type": "bonus_damage_if_cc", "value": 3.50, "scaling": { "type": "per_alpha_level", "value": 0.01 } }] }
+    }
   }
 };
